7946dfb7f86ca23c63960b7e1ed8ba22c15b50e6
sentresik <sentresik@gmail.com>
2024-01-13 00:10:59 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Django-App',
+    version='1.1',
+    packages=['pizza', 'pizza.migrations'],
+    url='',
+    license='',
+    author='SEBASTIAN',
+    author_email='',
+    description=''
+)
