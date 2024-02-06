d911fb280d5fa48491d7e7276bad3804adee6aad
sentresik <sentresik@gmail.com>
2024-02-07 00:22:39 +0100

    Revert "docker mySQL - in progress"
    This reverts commit c53ead3b2e60436a2338bf63a3719a3cc099704f.

--- App/App/settings.py
+++ App/App/settings.py
@@ -76,15 +76,8 @@ WSGI_APPLICATION = 'App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my_database',
-        'USER': 'root',
-        'PASSWORD': 'pass',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- App/data.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS my_database;
-
-USE my_database;
-
