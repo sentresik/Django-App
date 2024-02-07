ba5dcdcc176879d0ee3418294e4049b698012cf2
sentresik <sentresik@gmail.com>
2024-02-08 00:59:12 +0100

    Working login/register and some CSS

--- App/App/urls.py
+++ App/App/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.AllToDos.as_view(), name='index')
+    path('', views.index, name='index'),
+    path('login/', views.login_page, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- App/App/views.py
+++ App/App/views.py
@@ -1,14 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import ToDoItem
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from .forms import MyRegistrationForm
 
 
 # Create your views here.
-class AllToDos(ListView):
-    model = ToDoItem
-    template_name = "index.html"
+def index(request):
+    return render(request, 'index.html')
 
+def login_page(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('your-desired-redirect-url')
+        else:
+            pass
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
-# def index(request): #new
-#     return HttpResponse('<h1>Django Include URLs</h1>')
+def register(request):
+    if request.method == "POST":
+        form = MyRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    else:
+        form = MyRegistrationForm()
+    return render(request, 'register.html', {'form': form})
