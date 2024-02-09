c5cbb0c7fd08337bdd0662131b14647450936799
sentresik <sentresik@gmail.com>
2024-02-09 04:34:56 +0100

    Działa: Logout Usuwanie List (wraz z itemami) Listowanie Itemów Dodawanie Itemów Oznaczanie Itemów jako complete (obecnie ukrywam itemy gdzie is_completed = True bo łatwiej tak)

--- App/App/views.py
+++ App/App/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from .models import ToDoItem, ToDoList
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from .forms import MyRegistrationForm
 from django.contrib.auth.decorators import login_required
 
@@ -37,6 +37,11 @@ def register(request):
     return render(request, 'register.html', {'form': form})
 
 @login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect to login page after logout
+
+@login_required
 def list_view(request):
     lists = ToDoList.objects.filter(user=request.user)
     return render(request, 'list_view.html', {'lists': lists})
@@ -56,3 +61,29 @@ def create_list_view(request):
         return redirect('list_detail_view', list_id=list.id)
     else:
         return render(request, 'create_list_view.html')
+
+@login_required
+def create_item_view(request, list_id):
+    if request.method == 'POST':
+        text = request.POST['text']
+        list_id = request.POST['list_id']
+        list = get_object_or_404(ToDoList, pk=list_id, user=request.user)
+        item = ToDoItem.objects.create(text=text, list=list)
+        return redirect('list_detail_view', list_id=list.id)
+    else:
+        list = get_object_or_404(ToDoList, pk=list_id, user=request.user)
+        return render(request, 'create_item_view.html', {'list': list})
+
+@login_required
+def mark_item_complete_view(request, list_id, item_id):
+    item = get_object_or_404(ToDoItem, pk=item_id, list__pk=list_id)
+    item.is_completed = True
+    item.save()
+    # Redirect to list detail view for the same list
+    return redirect('list_detail_view', list_id=list_id)
+
+@login_required
+def delete_list_view(request, list_id):
+  list = get_object_or_404(ToDoList, pk=list_id)
+  list.delete()
+  return redirect('list_view')
